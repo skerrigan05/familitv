49830befe7db4c05e1a6b3d312a23ff40606a8c7
skerrigan05 <edgardsoto5@gmail.com>
2026-06-15 11:58:20 -0400

    Actualizar URL del tunel: https://experiences-style-album-belle.trycloudflare.com

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // URL base del backend de FamiliTV (túnel de Cloudflare)
 // Este archivo se actualiza automáticamente al iniciar start-all.bat
 window.FAMILITV = window.FAMILITV || {};
-window.FAMILITV.BASE_URL = 'https://pupils-voters-creates-invention.trycloudflare.com';
+window.FAMILITV.BASE_URL = 'https://experiences-style-album-belle.trycloudflare.com';
