93dbb4b51cd7d08588bcca3e0edcdb046f7992cd
skerrigan05 <edgardsoto5@gmail.com>
2026-06-15 10:48:42 -0400

    Actualizar URL del tunel: https://matching-correct-purchases-furthermore.trycloudflare.com

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // URL base del backend de FamiliTV (túnel de Cloudflare)
 // Este archivo se actualiza automáticamente al iniciar start-all.bat
 window.FAMILITV = window.FAMILITV || {};
-window.FAMILITV.BASE_URL = 'https://ferrari-devices-cardiac-wagon.trycloudflare.com';
+window.FAMILITV.BASE_URL = 'https://matching-correct-purchases-furthermore.trycloudflare.com';
